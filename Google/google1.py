c7e15fc02ade36f2907d1a2efb729fb541bbb102
Adrita Roy <47496055+adroy98@users.noreply.github.com>
2019-02-17 13:05:03 +0530

    Update google1.py

--- Google/google1.py
+++ Google/google1.py
@@ -1,9 +1,8 @@
 def verify(numbers, k):
-    for i in range(len(numbers)):
-        for j in range(i+1, len(numbers)):
-            if numbers[i] + numbers[j] == k:
-                print(True)
-                break
+    if(any(numbers[i]+numbers[j] == k for i in range(len(numbers)) for j in range(i+1, len(numbers)))):
+        print(True)
+    else
+        print(False)
 
 if __name__ == '__main__':
     numbers = list(map(float, input().split()))
